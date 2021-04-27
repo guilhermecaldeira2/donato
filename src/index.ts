545a50a28aa841bc7f0c29f7c69554d6aefabe57
guitdev <guilherme.silva@weduu.com>
2021-04-27 15:09:14 -0300

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import TelegrafMenu from './Menu';
 import telegrafStage from './Stage';
 import TelegrafKeyboard from './TelegrafKeyboard';
+
 export { TelegrafMenu, telegrafStage, TelegrafKeyboard };
 
 const chalk = require('chalk');
